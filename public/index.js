4f7dd1427a1eca74c5c9b3dae7cf3c5af6b69356
Ryan Romano <ryanaromano@gmail.com>
2018-02-17 19:59:58 -0500

    styled

--- public/index.js
+++ public/index.js
@@ -1,55 +1,23 @@
-/* JS goes here */
-let allShows;
-
+//////////////////////////////////////////////////////////////////////////////////////////
+// Makes a request for Show Data, sets current show
 $('document').ready(function() {
-    //grab url parts
-    let getUrl = window.location;
-    let baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-    let actualURL = window.location.href;
-    let pathID = actualURL.split("=")[1];
-
+    let pathID = getParameterByName("id",window.location);
+    let allShows;
 
     $.ajax({
         type : 'GET',
-        url : 'http://localhost:3000/shows',
+        url : '/shows',
         success : function(response) {
+
+            // Populate HTML elements with data
             handleShowsLoaded(response);
-            let numberCaption = document.createElement('h2');
 
-            //Land on home -> render 1st show, else render by id
-            if(actualURL == baseUrl){                
-                //toggle active class
-                $('#shows-list li').removeClass('active');
-                $('#shows-list li').first().addClass('active');
-                
-                //Create caption for first show in show selector
-                numberCaption.innerHTML = $('#' + response[0].id).index() + 1;
-                numberCaption.setAttribute("id", "after");
-                let selectedShow = document.getElementById(response[0].id);
-                selectedShow.appendChild(numberCaption);
-                // document.getElementById("#shows-list-container").scrollTo(0,0);
-                setSelectedShow(response[0].id);
-
-            } else {
-                //toggle active class
-                $('#shows-list li').removeClass('active');
-                $('#'+pathID).addClass('active');
-
-                let $container = $('#shows-list-container');
-                let $scrollTo = $('#' + pathID);
-
-                $container.scrollLeft(
-                    $scrollTo.offset().left - $container.offset().left + $container.scrollLeft()
-                );
-
-                //Create number caption under active show selector
-                numberCaption.innerHTML = $('#' + pathID).index() + 1;
-                numberCaption.setAttribute("id", "after");
-                let selectedShow = document.getElementById(pathID);
-                selectedShow.appendChild(numberCaption);
-
-                setSelectedShow(pathID);
+            // Checks URL param for current ID, sets selected show with corresponding ID
+            let activeShowId = response[0].id;
+            if (pathID != null && pathID != "") {
+              activeShowId = pathID;
             }
+            setSelectedShow(activeShowId)
         },
         error : function(xhr, textStatus, errorThrown) {
             console.log("error");
@@ -57,32 +25,36 @@ $('document').ready(function() {
     });
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////
+// Creates List Items and Assigns id to each. On LI click, set the URL with ID param "?=idHere"
 function handleShowsLoaded(shows){
-
     allShows = shows;
     for(let i=0; i < allShows.length; i++){
-        //Create LI with id=show.id
+
+        // Create LI with id=show.id
         let id = allShows[i].id;
         let ul = document.getElementById("shows-list");
         let li = document.createElement("li");
         li.setAttribute("id", id);
 
-        //Get base url
+        // et base url
         let getUrl = window.location;
         let baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
-        //save current position, href to new url, set show
+        // Save current position, href to new url, set show
         li.onclick = function() {
             location = (baseUrl + "?id=" + id);
             window.location =  baseUrl + '?id=' + id;
             setSelectedShow(id);
         };
-        //add to show selector list of [] [] [] ...
+
+        // Add to show selector list of [] [] [] ...
         ul.appendChild(li);
     }
 }
 
-//Get show
+//////////////////////////////////////////////////////////////////////////////////////////
+// Returns show based on provided ID
 function getShowByID(showID){
     for(let i=0; i < allShows.length; i++){
         if(allShows[i].id == showID){
@@ -91,22 +63,75 @@ function getShowByID(showID){
     }
 }
 
-function setSelectedShow(showID) {
+//////////////////////////////////////////////////////////////////////////////////////////
+// Fills html elements with data from show, from given show ID
+function setSelectedShow(pathID) {
+    setPageState(pathID);
+    let show = getShowByID(pathID);
 
-    let show = getShowByID(showID);
-
-    //Get reference to HTML to fill
+    // Get reference to HTML to fill elements
     let imageElement = document.getElementById("selected-show-image");
     let episodesElement = document.getElementById("selected-show-episodes");
     let titleElement = document.getElementById("selected-show-title");
 
-    //Grab data from show
+    // Grab data from show
     let showImage = show.product_image_url;
     let showEpisodes = show.episodes;
     let showTitle = show.title;
 
-    //Set data
+    // Set data
     imageElement.setAttribute("src", showImage);
     episodesElement.innerHTML = showEpisodes + " EPISODES";
     titleElement.innerHTML = showTitle.toUpperCase();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// https://stackoverflow.com/a/17086002
+// returns segment of URL ("id", url) - returns a9/d6/b8,etc
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Sets active list item in show selector & inserts index beneath it. 
+// Scrolls to active item on reload
+function setPageState(pathID){
+    let numberCaption = document.createElement('h2');
+    if(pathID == ""){                
+        //toggle active class
+        $('#shows-list li').removeClass('active');
+        $('#shows-list li').first().addClass('active');
+        
+        //Create caption for first show in show selector
+        numberCaption.innerHTML = $('#' + response[0].id).index() + 1;
+        numberCaption.setAttribute("id", "after");
+        let selectedShow = document.getElementById(response[0].id);
+        selectedShow.appendChild(numberCaption);
+        
+    } else {
+        //toggle active class
+        $('#shows-list li').removeClass('active');
+        $('#'+pathID).addClass('active');
+
+        //Get target container and target scroll location
+        let $container = $('#shows-list-container');
+        let $scrollTo = $('#' + pathID);
+
+        //Scroll to target
+        $container.scrollLeft(
+            $scrollTo.offset().left - $container.offset().left - 145
+        );
+
+        //Create number caption under active show selector
+        numberCaption.innerHTML = $('#' + pathID).index() + 1;
+        numberCaption.setAttribute("id", "after");
+        let selectedShow = document.getElementById(pathID);
+        selectedShow.appendChild(numberCaption);
+    }
 }
